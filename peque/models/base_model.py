9f42232e78d12e95838e860f26eeb339295c715f
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-06-27 12:33:20 -0700

    Added some more files

--- peque/models/base_model.py
+++ peque/models/base_model.py
@@ -2,15 +2,29 @@
 """ Class definition """
 import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
     """ Class BaseModel """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ intialize instance """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        if kwargs is not None and kwargs is not {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+            else:
+                dict = self.to_dict
+                storage.new(dict)
 
     def __str__(self):
         """ overrides str method """
@@ -20,6 +34,7 @@ class BaseModel:
         """ updates the public instance attribute updated_at with the
         current datetime """
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__
---
+++ peque/models/engine/file_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Class definition """
+import json
+
+
+class FileStorage:
+    """ Serializes instances to a JSON file and deserializes
+    JSON file to instances """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ returns the dictionary __objects """
+        return FileStorage.__objects
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
+        FileStorage.__objects = {f"{obj.__class__.__name__}.{id}":obj}
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path) """
+        with open(self.__file_path, mode="w", encoding="utf-8") as f:
+            json.dump(self.__objects, f)
+    def reload(self):
+        """ deserializes the JSON file to __objects """
+        try:
+            with open(self.__file_path, encoding="utf-8") as f:
+                self.__objects = json.load(f)
+        except Exception:
+            pass
