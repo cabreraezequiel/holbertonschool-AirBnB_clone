1b82e6d8ef0b2278c9b369778181e2669bfe3594
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-06-27 18:33:43 -0700

    Updated some files

--- peque/console.py
+++ peque/console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
 """ Class definition """
 
 
@@ -15,6 +16,37 @@ class HBNBCommand(cmd.Cmd):
         """EOF to exit the program"""
         exit()
 
+    def emptyline(self):
+        """If a line is empty it does nothing"""
+        pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it and prints the id"""
+        if len(arg.split()) == 0:
+            print("** class name missing **")
+        else:
+            try:
+                new_instance = (eval(arg.split()[0]))()
+                new_instance.save()
+                print(new_instance.id)
+            except NameError:
+                print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name and id"""
+        if len(arg.split()) == 0:
+            print("** class name missing **")
+        elif len(arg.split()) > 0:
+            try:
+                eval(arg.split()[0])
+            except NameError:
+                print("** class doesn't exist **")
+                return
+        if len(arg.split()) < 2:
+            print("** instance id missing **")
+        else:
+            try
+        
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
