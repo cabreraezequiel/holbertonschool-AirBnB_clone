a0c0397ac5ce253e17bf360de8f296ffee922241
Agustin Labadie <4433@holbertonschool.com>
2022-07-01 14:52:19 -0700

    Laba working

--- laba/models/base_model.py
+++ laba/models/base_model.py
@@ -2,40 +2,46 @@
 """ Class definition """
 import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
     """ Class BaseModel """
     def __init__(self, *args, **kwargs):
         """ intialize instance """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        if kwargs is not None and kwargs is not {}:
-            for key in kwargs:
+        if kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.datetime.strptime(
+                    value = datetime.datetime.strptime(
                             kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.datetime.strptime(
-                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+                    value = datetime.datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            #len(kwargs) == 0 or kwargs is None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ overrides str method """
-        return(f"[{__class__.__name__}] ({self.id}) {self.__dict__}")
+        print_dic = {k: self.__dict__[k] for k in self.__dict__ if k != '__class__'}
+        return(f"[{self.__class__.__name__}] ({self.id}) {print_dic}")
 
     def save(self):
         """ updates the public instance attribute updated_at with the
         current datetime """
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__
         of the instance """
-        self.__dict__["__class__"] = __class__.__name__
-        self.__dict__["created_at"] = (self.__dict__["created_at"]).isoformat("T")
-        self.__dict__["updated_at"] = (self.__dict__["updated_at"]).isoformat("T")        
-        return self.__dict__
-
+        dic = {key: value for key, value in self.__dict__.items()}
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        dic['__class__'] = self.__class__.__name__
+        return dic
--- laba/models/engine/file_storage.py
+++ laba/models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Class definition """
 import json
-from models.user import User
 
 
 class FileStorage:
@@ -16,17 +15,34 @@ class FileStorage:
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
+#        print(f"Printing obj {obj}")
         self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+    @staticmethod
+    def classes():
+        from models.base_model import BaseModel
+        class_dict = {'BaseModel' : BaseModel}
+        return class_dict
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path) """
+        from models.base_model import BaseModel
+        to_dicted = self.__objects.copy()
+        for k, v in to_dicted.items():
+            to_dicted[k] = v.to_dict()
         with open(self.__file_path, mode="w", encoding="utf-8") as f:
-            json.dump(self.__objects, f, default=str)
-
+            json.dump(to_dicted, f)
+    
     def reload(self):
         """ deserializes the JSON file to __objects """
+        from models.base_model import BaseModel
         try:
             with open(self.__file_path, encoding="utf-8") as f:
-                self.__objects = json.load(f)
+                data = json.loads(f.read())
+                class_dict = self.classes()
+#                print(type(self.__objects))
+#                print(f"Hola aca {self.__objects} Hola aca tambien")
+                for k, v in data.items():
+                    new_instance = class_dict[(k.split('.')[0])](**v)
+                    self.__objects[k] = new_instance
         except Exception:
             pass
