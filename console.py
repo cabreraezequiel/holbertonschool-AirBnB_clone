fab82aea85af7ea0c3523ecb7341bf45a3004971
Ezequiel Cabrera <98030236+cabreraezequiel@users.noreply.github.com>
2022-07-02 13:05:24 -0300

    Update console.py

--- console.py
+++ console.py
@@ -2,14 +2,7 @@
 """ Class definition """
 import cmd
 import re
-from models.base_model import BaseModel
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
