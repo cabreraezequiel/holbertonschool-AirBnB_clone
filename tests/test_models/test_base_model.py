db9dec97d6e33186a29982645389156545569f7e
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-02 14:12:35 -0700

    Added some test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,17 @@
 from models.base_model import BaseModel
+from datetime import datetime
 import unittest
-from model.base_model import BaseModel
 
 class BaseModelTest(unittest.TestCase):
-    test1 = BaseModel()
-    def test_0(self):
-        self.assertTrue(True)
-    def fun_not_run(self):
-        print("no run")
+
+    def test_types(self):
+        base_1 = BaseModel()
+        self.assertIs(type(base_1.id), str)
+        self.assertIs(type(base_1.created_at), datetime)
+        self.assertIs(type(base_1.updated_at), datetime)
+
+    @unittest.expectedFailure
+    def test_unique_id(self):
+        base_1 = BaseModel()
+        base_2 = BaseModel()
+        self.assertEqual(base_1.id(), base_2.id())
