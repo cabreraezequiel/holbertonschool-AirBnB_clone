b2b1d2fc51353aa13915bab6d4239a385b0b4bb7
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-02 11:39:51 -0700

    Update console.py

--- console.py
+++ console.py
@@ -9,24 +9,6 @@ class HBNBCommand(cmd.Cmd):
     """ Class HBNBCommand that inherits from cmd """
     prompt = '(hbnb)'
 
-    def __init__(self):
-        """Initializes"""
-        cmd.Cmd.__init__(self)
-        self.aliases = {
-            'show': self.do_show}
-#            'count': self.do_count}
-
-    def default(self, line):
-        """Default"""
-        #       arg = (line.replace('(', '.')).split('.')
-        pattern = r"[(.)]"
-        arg = re.split(pattern, line)
-        #        print(arg)
-        if len(arg) > 3 and arg[1] in self.aliases:
-            self.aliases[arg[1]](f"{str(arg[0])} {str(arg[2])[1:-1]}")
-        else:
-            print("*** Unknown syntax: %s" % line)
-
 #    def default(self, line):
 #        """Default"""
 ##       arg = (line.replace('(', '.')).split('.')
@@ -53,7 +35,9 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """Creates a new instance of BaseModel, saves it and prints the id. Usage create \33[31m<class name>\33[30m"""
+        """Creates a new instance of BaseModel, saves it and prints the id. \
+Usage: \033[92mcreate <class name>\033[0m"""
+
         if len(arg.split()) == 0:
             print("** class name missing **")
         else:
@@ -66,8 +50,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         """Prints the string representation of an instance based on the \
-class name and id. Usage \33[31mshow <class name> <id>\33[0m or \33[31m<class \
-name>.show(<id>)\33[0m"""
+class name and id. Usage: \033[92mshow <class name> <id>\33[0m or \033[92m<\
+class name>.show(<id>)\33[0m"""
 
         if len(arg.split()) == 0:
             print("** class name missing **")
@@ -104,7 +88,10 @@ name>.show(<id>)\33[0m"""
 #            print(count)
 
     def do_all(self, arg):
-        """Prints all string representation of all instances based or not on the class name"""
+        """Prints all string representation of all instances based or not on \
+the class name. Usage: \033[92mall \033[91m<class name>\33[0m or \033[92m<\
+class name>.all\33[0m --\033[91m[OPTIONAL]\33[0m"""
+
         dic = []
         if len(arg.split()) < 1:
             for value in storage.all().values():
