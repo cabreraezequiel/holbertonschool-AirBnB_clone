7db0c8f6a5d7b07c819139f03410b73bfaba2974
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-03 09:18:51 -0700

    Added more tests for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,8 +10,28 @@ class BaseModelTest(unittest.TestCase):
         self.assertIs(type(base_1.created_at), datetime)
         self.assertIs(type(base_1.updated_at), datetime)
 
-    @unittest.expectedFailure
     def test_unique_id(self):
         base_1 = BaseModel()
         base_2 = BaseModel()
-        self.assertEqual(base_1.id(), base_2.id())
+        test_id = base_2.id
+        self.assertNotEqual(base_1.id, base_2.id)
+        self.assertEqual(base_2.id, test_id)
+
+    def test_str(self):
+        base_1 = BaseModel()
+        self.assertEqual(base_1.__str__(), f"[BaseModel] ({base_1.id}) {base_1.__dict__}")
+
+    def test_save(self):
+        base_1 = BaseModel()
+        update = base_1.updated_at
+        self.assertEqual(base_1.updated_at, update)
+        base_1.save()
+        self.assertNotEqual(base_1.updated_at, update)
+
+    def test_to_dict(self):
+        base_1 = BaseModel()
+        self.assertIs(type(base_1.to_dict()), dict)
+        self.assertIn('__class__', base_1.to_dict())
+        self.assertIn('id', base_1.to_dict())
+        self.assertIn('created_at', base_1.to_dict())
+        self.assertIn('updated_at', base_1.to_dict())
