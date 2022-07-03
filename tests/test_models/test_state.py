9c74b9255aa5e81161969f1d17ddba86dce83780
Ezequiel Cabrera <98030236+cabreraezequiel@users.noreply.github.com>
2022-07-03 15:01:12 -0300

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,30 @@
+from models.State import State
+import unittest 
+"""Tests for State class"""
 
+
+class StateTest(unittest.TestCase):
+    """Unittests for State"""
+
+    def test_types(self):
+        """ Test type of attributes"""
+        state_1 = State()
+	self.assertIs(type(State.name), str)
+
+    def test_attributes(self):
+        """ Test attributes of State class"""
+        state_1 = State()
+        self.assertEqual(state_1.name, "")
+        self.assertIs(state_1.name), empty)
+        state_1.name = "test"
+        self.assertEqual(user_1.name, "test")
+
+    def test_to_dict(self):
+        """ Test to_dict() method"""
+        state_1 = State()
+        user_1.state = "test"
+        self.assertIn('__class__', state_1.to_dict())
+        self.assertIn('id', user_1.to_dict())
+        self.assertIn('created_at', user_1.to_dict())
+        self.assertIn('updated_at', user_1.to_dict())
+        self.assertIn('state', user_1.to_dict())
