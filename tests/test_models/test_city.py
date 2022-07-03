a23323050b7deafdbe8b59cbd41785241eba5fc5
Ezequiel Cabrera <98030236+cabreraezequiel@users.noreply.github.com>
2022-07-03 15:10:46 -0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,1 +1,35 @@
+from models.city import City
+import unittest
+"""Tests for City class"""
 
+
+class CityTest(unittest.TestCase):
+    """Unittests for City"""
+
+    def test_types(self):
+        """ Test type of attributes"""
+        city_1 = City()
+        self.assertIs(type(City.name), str)
+	self.assertIs(type(City.state_id), str)
+
+    def test_attributes(self):
+        """ Test attributes of City class"""
+        city_1 = City()
+        self.assertEqual(city_1.name, "")
+        self.assertEqual(city_1.state_id, "")
+        city_1.name = "test"
+        city_1.state_id = "test"
+        self.assertEqual(city_1.name, "test")
+        self.assertEqual(city_1.state_id, "test")
+
+    def test_to_dict(self):
+        """ Test to_dict() method"""
+        city_1 = City()
+        city_1.name = "test"
+        city_1.state_id = "test"
+        self.assertIn('__class__', city_1.to_dict())
+        self.assertIn('id', city_1.to_dict())
+        self.assertIn('created_at', city_1.to_dict())
+        self.assertIn('updated_at', city_1.to_dict())
+        self.assertIn('name', city_1.to_dict())
+        self.assertIn('state_id', city_1.to_dict())
