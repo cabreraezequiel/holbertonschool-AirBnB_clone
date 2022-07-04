5f3c2c44f652bb83ba73db318d3b2ab1424c53c7
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-07-04 16:39:17 -0700

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,4 +1,5 @@
 from console import HBNBCommand
+import re
 import sys
 import unittest
 from unittest.mock import patch
@@ -12,25 +13,45 @@ class HBNBCommandTest(unittest.TestCase):
     def test_quit(self):
         """ Test quit command """
         with patch('sys.stdout', new=StringIO()) as f:
-                HBNBCommand().onecmd("quit")
+            HBNBCommand().onecmd("quit")
         self.assertTrue(f)
         self.assertEqual(f.getvalue(), "")
 
     def test_EOF(self):
         """ Test EOF command """
         with patch('sys.stdout', new=StringIO()) as f:
-                HBNBCommand().onecmd("EOF")
+            HBNBCommand().onecmd("EOF")
         self.assertTrue(f)
+        self.assertEqual(f.getvalue(), "")
 
     def test_help(self):
         """ Test EOF command """
         with patch('sys.stdout', new=StringIO()) as f:
-                HBNBCommand().onecmd("help")
+            HBNBCommand().onecmd("help")
         self.assertTrue(f)
         self.assertIsNotNone(f.getvalue())
         self.assertNotEqual(f.getvalue(), "")
+        self.assertEqual(f.getvalue(), "\nDocumented commands (type help \
+<topic>):\n========================================\nEOF  all  count  create\
+  destroy  help  quit  show  update\n\n")
         with patch('sys.stdout', new=StringIO()) as f_1:
-                HBNBCommand().onecmd("help show")
+            HBNBCommand().onecmd("help show")
         self.assertTrue(f_1)
         self.assertIsNotNone(f_1)
         self.assertNotEqual(f_1.getvalue(), "")
+        self.assertEqual(f_1.getvalue(), "Prints the string representation of \
+an instance based on the class name and id. Usage: \033[92mshow <class name> \
+<id>\33[0m or \033[92m<class name>.show(<id>)\33[0m\n""")
+
+    def test_emptyline(self):
+        """ Test empty line """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("")
+        self.assertEqual(f.getvalue(), "")
+
+    def test_createBaseModel(self):
+        """ Test create BaseModel """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+        self.assertNotEqual(f.getvalue(), "")
+        self.assertIsNotNone(f.getvalue())
