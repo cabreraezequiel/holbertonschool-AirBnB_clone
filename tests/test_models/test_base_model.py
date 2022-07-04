0d061c052d8f997ecb8dd189e5f9987cddee1b37
Agustin Labadie <98338462+Labalabadie@users.noreply.github.com>
2022-07-04 10:15:16 -0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,17 +1,20 @@
 from models.base_model import BaseModel
 import unittest
 from datetime import datetime
+"""Test Module for BaseModel class"""
 
 
 class BaseModelTest(unittest.TestCase):
-
+"""Test_class for BaseModel"""
     def test_types(self):
+        """Test the Type is correct"""
         base_1 = BaseModel()
         self.assertIs(type(base_1.id), str)
         self.assertIs(type(base_1.created_at), datetime)
         self.assertIs(type(base_1.updated_at), datetime)
 
     def test_unique_id(self):
+        """Tasts the id is unique"""
         base_1 = BaseModel()
         base_2 = BaseModel()
         test_id = base_2.id
@@ -19,11 +22,13 @@ class BaseModelTest(unittest.TestCase):
         self.assertEqual(base_2.id, test_id)
 
     def test_str(self):
+        """Tests the __str__ method"""
         base_1 = BaseModel()
         self.assertEqual(base_1.__str__(),
                          f"[BaseModel] ({base_1.id}) {base_1.__dict__}")
 
     def test_save(self):
+        """Tests saving method"""
         base_1 = BaseModel()
         update = base_1.updated_at
         self.assertEqual(base_1.updated_at, update)
@@ -31,6 +36,7 @@ class BaseModelTest(unittest.TestCase):
         self.assertNotEqual(base_1.updated_at, update)
 
     def test_to_dict(self):
+        """Tests setting instances into dictionary for JSON serialization"""
         base_1 = BaseModel()
         self.assertIs(type(base_1.to_dict()), dict)
         self.assertIn('__class__', base_1.to_dict())
